d6d1b149d90f60cd7d5c853e66effff7594005b8
Manish Kumar Verma <kumarvermam361@gmail.com>
2026-07-28 04:37:14 +0530

    Time: 0 ms (100%), Space: 17.4 MB (79.16%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     bool solve(int i, vector<int>& cvis, vector<vector<int>>& graph){
         int n = graph.size();
-        queue<int> q;
-        cvis[i] = 1;
-        q.push(i);
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            for(auto temp : graph[node]){
-                if(cvis[temp] == -1 && cvis[node] == 1){
-                    cvis[temp] = 2;
-                    q.push(temp);
-                }
-                else if(cvis[temp] == -1 && cvis[node] == 2){
-                    cvis[temp] = 1;
-                    q.push(temp);
-                }
-                else if(cvis[temp] != -1 && cvis[node] == cvis[temp]){
+        if(cvis[i] == -1){
+            cvis[i] = 1;
+        //     if(!solve(i, cvis, graph))
+        //         return false;
+        }
+        for(auto temp : graph[i]){
+            if(cvis[temp] == -1 && cvis[i] == 1){
+                cvis[temp] = 2;
+                if(!solve(temp, cvis, graph)){
                     return false;
                 }
+                
+            }
+            else if(cvis[temp] == -1 && cvis[i] == 2){
+                cvis[temp] = 1;
+                if(!solve(temp, cvis, graph)){
+                    return false;
+                }
+            }
+            else if(cvis[temp] != -1 && cvis[i] == cvis[temp]){
+                return false;
             }
         }
         return true;
