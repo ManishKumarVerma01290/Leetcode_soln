17e04342b4847f33f3a7de0d3c4c0966771b2108
Manish Kumar Verma <kumarvermam361@gmail.com>
2026-07-29 01:19:26 +0530

    Time: 0 ms (100%), Space: 7.9 MB (74.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,7 +5,7 @@ public:
         memset(t, -1, sizeof(t));
         t[1] = 1;
         t[2] = 2;
-        for(int i = 3; i < n + 1; i++){
+        for(int i = 3; i < 46; i++){
             t[i] = t[i-1] + t[i-2];
         }
         return t[n];
