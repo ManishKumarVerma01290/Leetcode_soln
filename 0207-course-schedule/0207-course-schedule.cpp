6ae855c3c42e66faee5074ffbf915a12a1d5298a
Manish Kumar Verma <kumarvermam361@gmail.com>
2026-07-30 03:14:50 +0530

    Time: 10 ms (23.72%), Space: 19.9 MB (39.4%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,70 +1,30 @@
 class Solution {
 public:
-    bool dfs_cycle(int i, vector<int>& vis, vector<int>& pathvis, vector<vector<int>>& adj){
-        vis[i] = 1;
-        pathvis[i] = 1;
-        for(auto ni : adj[i]){
-            if(!vis[ni]){
-                if(dfs_cycle(ni, vis, pathvis, adj)){
-                    return true;
-                }
-            }
-            else if(pathvis[ni]){
-                return true;
-            }
-        }
-        pathvis[i] = 0;
-        return false;
-    }
-    void solve(int i, vector<int>& vis, vector<vector<int>>& adj, stack<int>& st){
-        vis[i] = 1;
-        for(auto ni : adj[i]){
-            if(!vis[ni]){
-                solve(ni, vis, adj, st);
-            }
-        }
-        st.push(i);
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> vis(numCourses, 0);
-        vector<int> pathvis(numCourses, 0);
+        vector<int> indegree(numCourses, 0);
         vector<vector<int>> adj(numCourses);
         for(auto i : prerequisites){
             adj[i[1]].push_back(i[0]);
+            indegree[i[0]]++;
         }
+        queue<int> q;
         for(int i = 0; i < numCourses; i++){
-            if(!vis[i]){
-                if(dfs_cycle(i, vis, pathvis, adj)){
-                    return false;
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        int count = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            count++;
+            for(auto ni : adj[node]){
+                indegree[ni]--;
+                if(indegree[ni] == 0){
+                    q.push(ni);
                 }
             }
         }
-        for(int i = 0; i < vis.size(); i++){
-            vis[i] = 0;
-        }
-        stack<int> st;
-        for(int i = 0; i < numCourses; i++){
-            if(!vis[i]){
-                solve(i, vis, adj, st);
-            }
-        }
-        // vector<int> temp;
-        // while(!st.empty()){
-        //     temp.push_back(st.top());
-        //     st.pop();
-        // }
-        // for(auto ch : prerequisites){
-        //     int i = ch[1];
-        //     int j = ch[0];
-        //     int l = 0, h = temp.size() - 1;
-        //     while(l < h){
-        //         if(temp[l] == i && temp[h] == j && l >= h){
-        //             return false;
-        //         }
-        //         l++;
-        //         h--;
-        //     }
-        // }
-        return true;
+        return count == numCourses;
     }
 };
