b8214eb5e3a027ca6ce4a8b2c1bbdd8541536669
Manish Kumar Verma <kumarvermam361@gmail.com>
2026-08-01 04:48:07 +0530

    Time: 4 ms (90.37%), Space: 52.8 MB (74.88%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<vector<int>> adj(n);
-        vector<int> indegree(n);
-        for(int i = 0; i < n; i++){
-            for(auto ni : graph[i]){
-                adj[ni].push_back(i);
-            }
-        }
-        for(int i = 0; i < n; i++){
-            for(auto ni : adj[i]){
-                indegree[ni]++;
-            }
-        }
-        queue<int> q;
-        for(int i = 0; i < n; i++){
-            if(indegree[i] == 0){
-                q.push(i);
-            }
-        }
-        vector<int> ans;
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            ans.push_back(node);
-            for(auto ni : adj[node]){
-                indegree[ni]--;
-                if(indegree[ni] == 0){
-                    q.push(ni);
+    bool check_cycle(int i, vector<int>& vis, vector<vector<int>>& graph, vector<int>& pathvis, vector<int>& temp){
+        vis[i] = 1;
+        pathvis[i] = 1;
+        for(auto ni : graph[i]){
+            if(!vis[ni]){
+                if(check_cycle(ni, vis, graph, pathvis, temp)){
+                    return true;
                 }
             }
+            else if(pathvis[ni]){
+                return true;
+            }
         }
-        sort(ans.begin(), ans.end());
-        return ans;
+        temp[i] = 1;
+        pathvis[i] = 0;
+        return false;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> vis(n, 0);
+        vector<int> pathvis(n, 0);
+        vector<int> temp(n, 0);
+        vector<int> safe;
+        for(int i = 0; i < n; i++){
+            if(!vis[i]){
+                check_cycle(i, vis, graph, pathvis, temp);
+            }
+        }
+        for(int i = 0; i < n; i++){
+            if(temp[i] == 1){
+                safe.push_back(i);
+            }
+        }
+        return safe;
     }
 };
