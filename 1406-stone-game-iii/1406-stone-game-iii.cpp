7bef538fd5fee7bad57defcd31de15953671f747
Manish Kumar Verma <kumarvermam361@gmail.com>
2026-08-03 17:25:27 +0530

    Time: 11 ms (90.58%), Space: 136.1 MB (76.04%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,30 +1,20 @@
 class Solution {
-    int solve(int i, vector<int>& stoneValue, vector<int>& dp){
-        int n = stoneValue.size();
-        if(i >= n){
-            return 0;
-        }
-        if(dp[i] != INT_MIN){
-            return dp[i];
-        }
-        int sum = 0;
-        int ans = INT_MIN;
-        for(int k = 0; k < 3 && i+k < n; k++){
-            sum += stoneValue[i + k];
-            ans = max(ans, sum - solve(i + k + 1, stoneValue, dp));
-        }
-        dp[i] = ans;
-        return dp[i];
-    }
 public:
     string stoneGameIII(vector<int>& stoneValue) {
         int n = stoneValue.size();
-        vector<int> dp(n, INT_MIN);
-        int diff = solve(0, stoneValue, dp);
-        if(diff > 0){
+        vector<int> dp(n + 1, 0);
+        for(int i = n - 1; i >= 0; i--){
+            int sum = 0;
+            dp[i] = INT_MIN;
+            for(int k = 0; k < 3 && i+k < n; k++){
+                sum += stoneValue[i + k];
+                dp[i] = max(dp[i], sum - dp[i + k + 1]);
+            }
+        }
+        if(dp[0] > 0){
             return "Alice";
         }
-        else if(diff < 0){
+        else if(dp[0] < 0){
             return "Bob";
         }
         return "Tie";
