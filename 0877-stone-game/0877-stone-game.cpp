8c8997f0385ccacbb4cce3c9e9cab92f81bdceb2
Manish Kumar Verma <kumarvermam361@gmail.com>
2026-08-03 18:39:08 +0530

    Time: 7 ms (36.56%), Space: 19.8 MB (11.02%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        vector<vector<int>> dp(n, vector<int> (n, 0));
+        for(int i = 0; i < n; i++){
+            dp[i][i] = piles[i];
+        }
+        for(int k = 2; k <= n; k++){
+            for(int i = 0; i + k - 1 < n; i++){
+                int j = i + k - 1;
+                dp[i][j] = max(piles[i] - dp[i+1][j], piles[j] - dp[i][j-1]);
+            }
+        }
+        return dp[0][n - 1] > 0;
     }
 };
