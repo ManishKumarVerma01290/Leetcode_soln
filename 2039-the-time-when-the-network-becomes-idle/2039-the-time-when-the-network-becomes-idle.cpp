a6388206c537b91ecff70db4795bf92a6a1ac0f9
Manish Kumar Verma <kumarvermam361@gmail.com>
2026-08-05 14:16:26 +0530

    Time: 212 ms (35.84%), Space: 209.4 MB (21.05%) - LeetHub

--- 2039-the-time-when-the-network-becomes-idle/2039-the-time-when-the-network-becomes-idle.cpp
+++ 2039-the-time-when-the-network-becomes-idle/2039-the-time-when-the-network-becomes-idle.cpp
@@ -2,29 +2,22 @@ class Solution {
 public:
     int networkBecomesIdle(vector<vector<int>>& edges, vector<int>& patience) {
         int n = patience.size();
-        vector<vector<pair<int, int>>> adj(n);
+        vector<vector<int>> adj(n);
         for(auto i : edges){
-            adj[i[0]].push_back({i[1], 1});
-            adj[i[1]].push_back({i[0], 1});
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
         }
-        vector<int> dist(n, INT_MAX);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> dist(n, -1);
+        queue<int> q;
         dist[0] = 0;
-        pq.push({0, 0});
-        while(!pq.empty()){
-            pair<int, int> temp = pq.top();
-            pq.pop();
-            int dis = temp.first;
-            int node = temp.second;
-            if(dis > dist[node]){
-                continue;
-            }
+        q.push(0);
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
             for(auto ni : adj[node]){
-                int nnode = ni.first;
-                int nwt = ni.second;
-                if(dis + nwt < dist[nnode]){
-                    dist[nnode] = dis + nwt;
-                    pq.push({dist[nnode], nnode});
+                if(dist[ni] == -1){
+                    dist[ni] = dist[node] + 1;
+                    q.push(ni);
                 }
             }
         }
