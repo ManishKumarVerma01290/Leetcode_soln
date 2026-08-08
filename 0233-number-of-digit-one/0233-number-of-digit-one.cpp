a046822738e60ca23ca8549162cf59011986348c
Manish Kumar Verma <kumarvermam361@gmail.com>
2026-08-08 19:50:07 +0530

    Time: 0 ms (100%), Space: 7.9 MB (55.71%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.cpp
+++ 0233-number-of-digit-one/0233-number-of-digit-one.cpp
@@ -1,55 +1,35 @@
 class Solution {
 public:
     using ll = long long;
-
     ll dp[2][12][12];
-
     ll dp_fxn(string &s, bool tight, ll idx, ll count) {
-
         if(idx == s.size()) {
             return count;
         }
-
         if(dp[tight][idx][count] != -1) {
             return dp[tight][idx][count];
         }
-
         ll lb = 0;
         ll ub = 9;
-
         if(tight == true) {
             ub = s[idx] - '0';
         }
-
         ll ans = 0;
-
         for(ll i = lb; i <= ub; i++) {
-
             ll ncount = count;
-
             if(i == 1) {
                 ncount++;
             }
-
             bool ntight = tight && (i == s[idx] - '0');
-
-            ans += dp_fxn(
-                s,
-                ntight,
-                idx + 1,
-                ncount
-            );
+            ans += dp_fxn(s, ntight, idx + 1, ncount);
         }
-
         return dp[tight][idx][count] = ans;
     }
 
     int countDigitOne(int n) {
-
         string a = to_string(n);
-
         memset(dp, -1, sizeof(dp));
-
-        return dp_fxn(a, true, 0, 0);
+        ll ans = dp_fxn(a, true, 0, 0);
+        return ans;
     }
 };
