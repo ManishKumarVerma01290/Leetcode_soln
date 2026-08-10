be5715086f48c56b07ff4cf6084bfb342fb7826a
Manish Kumar Verma <kumarvermam361@gmail.com>
2026-08-10 12:49:52 +0530

    Time: 0 ms (100%), Space: 7.9 MB (11.1%) - LeetHub

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.cpp
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
+    int dp[5][2][40];
     int dp_fxn(string& s, int idx, bool tight, int sum){
         if(idx == s.size()){
             return (sum % 2 == 0);
+        }
+        if(dp[idx][tight][sum] != -1){
+            return dp[idx][tight][sum];
         }
         int lb = 0;
         int ub = 9;
@@ -15,10 +19,11 @@ public:
             bool ntight = tight && (i == ub);
             ans += dp_fxn(s, idx + 1, ntight, nsum);
         }
-        return ans;
+        return dp[idx][tight][sum] = ans;
     }
     int countEven(int num) {
         string s = to_string(num);
+        memset(dp, -1, sizeof(dp));
         return dp_fxn(s, 0, 1, 0) - 1;
     }
 };
