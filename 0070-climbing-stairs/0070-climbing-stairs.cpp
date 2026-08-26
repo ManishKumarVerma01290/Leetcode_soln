be83e33fbde6010f393435b8f5247fb694875f39
Manish Kumar Verma <kumarvermam361@gmail.com>
2026-08-26 15:58:39 +0530

    Time: 0 ms (100%), Space: 7.8 MB (74.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int t[46];
     int solve(int n){
-        if(n == 1 || n == 2){
-            return n;
+        t[1] = 1;
+        t[2] = 2;
+        for(int i = 3; i <= n; i++){
+            t[i] = t[i-1] + t[i-2];
         }
-        if(t[n] != -1){
-            return t[n];
-        }
-        return t[n] = solve(n - 1) + solve(n - 2);
+        return t[n];
     }
     int climbStairs(int n) {
-        memset(t, -1, sizeof(t));
         return solve(n);
     }
 };
