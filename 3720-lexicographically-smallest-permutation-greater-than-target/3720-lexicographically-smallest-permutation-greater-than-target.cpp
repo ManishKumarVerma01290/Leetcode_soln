d975f66a02783071f52bd25939b15f3bbe824039
Manish Kumar Verma <kumarvermam361@gmail.com>
2026-08-27 23:07:25 +0530

    Time: 0 ms (100%), Space: 10.7 MB (62.42%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -2,121 +2,69 @@ class Solution {
 public:
     string lexGreaterPermutation(string s, string target) {
         map<char, int> freq;
-
-        for(auto i : s) {
+        for (auto i : s) {
             freq[i]++;
         }
-
         vector<int> num;
-        for(auto i : s) {
+        for (auto i : s) {
             num.push_back(i - 'a');
         }
-
         sort(num.begin(), num.end());
-
         int Min = num[0], Max = num[num.size() - 1];
-
-        // Case 1: smallest permutation itself is greater
-        if(Min > target[0] - 'a') {
+        if (Min > target[0] - 'a') {
             string ans = "";
-
-            for(auto i : num) {
+            for (auto i : num) {
                 ans += char(i + 'a');
             }
-
             return ans;
         }
-
-        // TERA LOGIC
         string ans = "";
-
-        for(int i = 0; i < target.size(); i++) {
-
-            // Same character mil gaya
-            if(freq.find(target[i]) != freq.end()) {
-
+        for (int i = 0; i < target.size(); i++) {
+            if (freq.find(target[i]) != freq.end()) {
                 ans += target[i];
-
                 freq[target[i]]--;
-
-                if(freq[target[i]] == 0) {
+                if (freq[target[i]] == 0) {
                     freq.erase(target[i]);
                 }
-            }
-
-            // Same character nahi mila
-            else {
-
-                // target[i] se bada smallest character
+            } else {
                 auto it = freq.upper_bound(target[i]);
-
-                if(it != freq.end()) {
-
+                if (it != freq.end()) {
                     ans += it->first;
-
                     it->second--;
-
-                    if(it->second == 0) {
+                    if (it->second == 0) {
                         freq.erase(it);
                     }
-
-                    // Remaining smallest
-                    while(!freq.empty()) {
+                    while (!freq.empty()) {
                         auto p = freq.begin();
-
                         ans += p->first;
-
                         p->second--;
-
-                        if(p->second == 0) {
+                        if (p->second == 0) {
                             freq.erase(p);
                         }
                     }
-
                     return ans;
                 }
-
-                // yahan direct bigger nahi mila
                 break;
             }
         }
-
-        // 🔥 Yahan tak target match ho gaya ya aage answer nahi bana
-        // Previous characters ko wapas lao aur upper_bound try karo
-
-        for(int i = min((int)ans.size(), (int)target.size()) - 1;
-            i >= 0; i--) {
-
+        for (int i = ans.size() - 1; i >= 0; i--) {
             freq[ans[i]]++;
-
             ans.pop_back();
-
             auto it = freq.upper_bound(target[i]);
-
-            if(it != freq.end()) {
-
+            if (it != freq.end()) {
                 ans += it->first;
-
-                it->second--;
-
-                if(it->second == 0) {
-                    freq.erase(it);
+                freq[it->first]--;
+                if (freq[it->first] == 0) {
+                    freq.erase(it->first);
                 }
-
-                // Remaining smallest
-                while(!freq.empty()) {
-
+                while (!freq.empty()) {
                     auto p = freq.begin();
-
                     ans += p->first;
-
                     p->second--;
-
-                    if(p->second == 0) {
+                    if (p->second == 0) {
                         freq.erase(p);
                     }
                 }
-
                 return ans;
             }
         }
