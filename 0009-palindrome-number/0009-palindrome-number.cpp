5f68903d1b9ef485b3b5b3e4f0beeee9b05a624a
Manish Kumar Verma <kumarvermam361@gmail.com>
2026-08-28 23:45:19 +0530

    Time: 0 ms (100%), Space: 10.9 MB (6.99%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
+        string s = to_string(x);
+        int i = 0, j = s.length() - 1;
+        while(i < j){
+            if(s[i] != s[j]){
+                return false;
+            }
+            i++;
+            j--;
         }
-
-        int reversed = 0;
-        int original = x;
-        
-        while (x > reversed) {
-            reversed = reversed * 10 + x % 10;
-            x /= 10;
-        }
-
-        return x == reversed || x == reversed / 10;
+        return true;
     }
 };
